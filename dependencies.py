74f867731b48e549403997baae4b61e045de66b2
Alex Solomonov <solomonoff.alexandr@gmail.com>
2026-06-02 22:56:56 +0300

    First commit with deploy fuction

---
+++ dependencies.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numpy import typing as npt
+import os
+import sys
+from pathlib import Path
+from tqdm import tqdm
+from glob import glob
+from scipy import interpolate
+
+
+### local packages
