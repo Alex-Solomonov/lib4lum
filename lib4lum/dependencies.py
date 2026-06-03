b7b5674bffb2be1a70a9178b41d4d3a04e817000
Alex Solomonov <solomonoff.alexandr@gmail.com>
2026-06-03 23:48:43 +0300

    Update dependencies.py

--- lib4lum/dependencies.py
+++ lib4lum/dependencies.py
@@ -13,3 +13,17 @@ from . import (
     phase_profiles,
     model_generator
 )
+
+###Auto-detect Lumerical and return the lumapi module
+search_roots = [
+    Path(os.environ.get("ProgramFiles", r"C:\Program Files")) / "Lumerical",
+    Path(os.environ.get("ProgramFiles(x86)", r"C:\Program Files (x86)")) / "Lumerical",
+]
+for root in search_roots:
+    api_paths = sorted(root.glob("v*/api/python"))
+    if api_paths:
+        sys.path.append(str(api_paths[-1]))
+        break
+else:
+    raise FileNotFoundError("Lumerical not found in Program Files")
+import lumapi
