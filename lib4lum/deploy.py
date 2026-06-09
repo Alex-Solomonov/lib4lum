10a9b08cd4e04608da2d938d586ce0fc75bfd27d
Alex Solomonov <solomonoff.alexandr@gmail.com>
2026-06-09 13:45:15 +0300

    Added Config Generator and Reader

--- lib4lum/deploy.py
+++ lib4lum/deploy.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from configparser import ConfigParser
 
 def run() -> None:
     '''
@@ -24,3 +24,48 @@ def run() -> None:
     GLOBAL_PATH = Path.cwd().parent
     MODELS_PATH = GLOBAL_PATH / 'models'
     MODELS_PATH.mkdir(parents = True, exist_ok = True)
+    _generate_config
+
+def _generate_config() -> None:
+    '''
+    '''
+    GLOBAL_PATH = Path.cwd().parent
+    MODELS_PATH = GLOBAL_PATH / 'models'
+    config = ConfigParser()
+    config.add_section('SOLVER')
+    config['SOLVER']['wavelength'] = '1e-09'
+
+    config.add_section('BOX')
+    config['BOX']['z_min'] = '-3e-09'
+
+    config.add_section('MODEL')
+    config['MODEL']['period'] = '425e-09'
+    
+    config.add_section('UNIT CELL')
+    config['UNIT CELL']['h_disk'] = '150e-9'
+    config['UNIT CELL']['h_spacer'] = '150e-9'
+    
+
+    with open(MODELS_PATH / 'model_config.ini', 'w') as config_file:
+        config.write(config_file)
+
+def read_config(config_path = None) -> dict:
+    '''
+    '''
+    if config_path is None:
+        GLOBAL_PATH = Path.cwd().parent
+        MODELS_PATH = GLOBAL_PATH / 'models'
+        config_path = MODELS_PATH / 'model_config.ini'
+
+    config = ConfigParser()
+    config.read(config_path)
+
+    params = {
+        section: {
+            key: float(value)
+            for key, value in config[section].items()
+        }
+        for section in config.sections()
+    }
+
+    return params
